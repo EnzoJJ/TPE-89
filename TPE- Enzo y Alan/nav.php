6faf61a1361a63ce1c47edad8055481be96ea12d
AlanCalles <114703593+AlanCalles@users.noreply.github.com>
2023-09-24 20:59:50 -0300

    Delete TPE- Enzo y Alan directory

--- TPE- Enzo y Alan/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Inicio</a></li>
-        <li><a href="estrenos.php">Estrenos</a></li>
-        <li><a href="reseñas.php">Reseñas</a></li>
-    </ul>
-</nav>
-<link rel="stylesheet" href="css/estilo.css">
