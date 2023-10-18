da4291e6e93947f2730efd55bb41cbc93fb5a86a
EnzoJJ <114018093+EnzoJJ@users.noreply.github.com>
2023-10-17 23:56:00 -0300

    Delete TPE- Enzo y Alan directory

--- TPE- Enzo y Alan/app/helpers/error.helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class ErrorHelper{
-        //Muestra un cuadro de error, recibe un String
-        public function showError($error){
-            include_once './templates/header.phtml';
-            require_once './templates/error.phtml';
-        }
-    }
-?>
--- TPE- Enzo y Alan/database/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-const MYSQL_HOST = 'localhost';
-const MYSQL_DB = 'db_pelicula';
-const MYSQL_USER = 'root';
-const MYSQL_PASS = '';
-
-?>
